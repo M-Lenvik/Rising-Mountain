d74666b87fea4bd002eb0e57a9d428f353069ae8
M-Lenvik <lenvik.marie@gmail.com>
2026-05-29 18:09:51 +0200

    Update base path in Vite configuration to match new naming convention

--- rising-mountain/frontend/vite.config.js
+++ rising-mountain/frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rising-mountain/',
+  base: '/Rising-Mountain/',
   plugins: [react()],
   server: {
     port: 5173,
